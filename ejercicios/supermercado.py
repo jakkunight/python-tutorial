ee0cb9cc4b49872aef46b3b2ab9fdc56925dc7ec
Jakku <J97k107u45k117n@gmail.com>
2023-03-06 11:19:33 -0300

    "Added supermercado.py"

---
+++ ejercicios/supermercado.py
@@ -0,0 +1,19 @@
+#/usr/bin/python
+# ANTES DE EMPEZAR:
+# Vamos a aprender sobre el uso de Git y GitHub para tener un "control de versiones" de
+# nuestro código. También vamos a planificar la solución de manera que sea más fácil usar
+# las herramientas.
+#
+# Problema del supermercado.
+# Supongamos que contamos con una lista de productos, con sus precios y existencias.
+# También contamos con otra lista (puede estar inicialmente vacía) de clientes.
+# Por supuesto, tenemos con una nómina de empleados que operan el negocio.
+# Hacer un programa que permita agregar, modificar y eliminar elementos de nuestras listas
+# y mostrarlos en pantalla.
+# 
+# No hace falta usar una Base de Datos para este caso, pero de todos modos vamos a crear un
+# software que les permita conocer lo básico acerca de como usar los datos provenientes de 
+# la misma.
+# SPOILER: Vamos a ver como conectarnos a una Base de Datos SQL desde Python si todo sale bien.
+# 
+# Desafío secreto: Implementar un sistema para gestionar nuestro "supermercado".
