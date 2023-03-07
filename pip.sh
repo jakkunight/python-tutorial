cffa0e3130e7c2204b42beaa13f49c0ddc083d0a
Jakku <J97k107u45k117n@gmail.com>
2023-03-07 16:14:55 -0300

    Added "pip" documentation file.

---
+++ pip.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+# Vamos a ver como usar pip para instalar módulos de Python.
+# Vamos a ver como instalarlo, suponiendo que Python ya está instalado.
+# En Linux:
+#     python -m ensurepip --upgrade
+# En Windows:
+#      py -m ensurepip --upgrade
+# En MacOS:
+#     python -m ensurepip --upgrade
+#
+# Para instalar un módulo, ejecutamos el siguiente comando.
+# En Linux:
+#     python -m pip install <nombre_módulo>
+# En Windows:
+#      py -m pip install <nombre_módulo>
+# En MacOS:
+#     python -m pip install <nombre_módulo>
+#
+# Y eso sería todo. No voy a entrar en detalles acerca de como publicar módulos,
+# al menos no por el momento.
